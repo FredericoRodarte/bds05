46db59cf792ac72fdd1eefe50de5fd19e055b142
Frederico Rodarte <fredericorodarte12@gmail.com>
2023-03-10 18:35:07 -0300

    Exceptions, validation, security

--- src/main/java/com/devsuperior/movieflix/entities/User.java
+++ src/main/java/com/devsuperior/movieflix/entities/User.java
@@ -1,6 +1,7 @@
 package com.devsuperior.movieflix.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Objects;
 import java.util.Set;
@@ -16,9 +17,12 @@ import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 @Entity
 @Table(name = "tb_user")
-public class User implements Serializable{
+public class User implements UserDetails,Serializable{
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -99,6 +103,32 @@ public class User implements Serializable{
 		return Objects.equals(id, other.id);
 	}
     
-    
+	@Override
+	public String getUsername() {
+		return email;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}	
+   
     
 }
